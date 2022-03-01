00e6c815ce6ba627294603d883fffba826a0f91f
mathieulj <mathieulj@users.noreply.github.com>
2022-03-01 10:16:16 -0500

    Query the environment directly instead of using libc's setlocale. (#1)
    * Query the environment directly instead of using libc's setlocale.
    
    * Fix no_std inner attribute. <3 clippy.
    
    * Cleanup new lint findings.
    
    * Revert "Cleanup new lint findings."
    
    We don't want to bump the MSRV.
    
    This reverts commit c4ae28df7983145feb5c2b716cd21a0fb3cc89d1.
    
    * Allow unknow lints for building on older rust versions.
    
    * Avoid mutating the environment during tests.

--- src/unix.rs
+++ src/unix.rs
@@ -1,56 +1,64 @@
-use alloc::string::String;
-use core::ptr::NonNull;
-use cstr_core::CStr;
+#![allow(unknown_lints)]
+use std::{env, ffi::OsStr};
 
+const LC_ALL: &str = "LC_ALL";
 const LC_CTYPE: &str = "LC_CTYPE";
+const LANG: &str = "LANG";
+
+/// Environment variable access abstraction to allow testing without
+/// mutating env variables.
+///
+/// Use [StdEnv] to query [std::env]
+trait EnvAccess {
+    /// See also [std::env::var]
+    fn get(&self, key: impl AsRef<OsStr>) -> Option<String>;
+}
+
+/// Proxy to [std::env]
+struct StdEnv;
+impl EnvAccess for StdEnv {
+    fn get(&self, key: impl AsRef<OsStr>) -> Option<String> {
+        env::var(key).ok()
+    }
+}
 
 pub(crate) fn get() -> Option<String> {
-    // SAFETY: `setlocale` is being called with a known category and an empty, null-terminated string.
-    let locale = NonNull::new(unsafe { libc::setlocale(libc::LC_ALL, b"\0".as_ptr().cast()) })?;
+    _get(&StdEnv)
+}
 
-    // SAFETY: `setlocale` returns a pointer to a NUL terminated string which
-    // is then cloned into an owned Rust string, and has otherwise been checked as non-null.
-    let code = unsafe { CStr::from_ptr(locale.as_ptr()) };
-    let code = code.to_str().ok()?;
+fn _get(env: &impl EnvAccess) -> Option<String> {
+    let code = env
+        .get(LC_ALL)
+        .or_else(|| env.get(LC_CTYPE))
+        .or_else(|| env.get(LANG))?;
 
-    parse_locale_code(code)
+    parse_locale_code(&code)
 }
 
 fn parse_locale_code(code: &str) -> Option<String> {
-    // Support two behaviors:
-    //
-    // - `LC_ALL` and `LANG` are set, so `setlocale` returns a single locale identifier.
-    // - `LANG` is set but `LC_ALL` is not, so `setlocale` returns a list of all identifiers.
-    //
-    // In the first case we have a clear case of which locale to use. In the second, we will fallback to using `LC_CTYPE`
-    // as its the most closely related to character localization.
-    let code = if !code.contains(LC_CTYPE) {
-        code
-    } else {
-        code.split(';')
-            .find(|c| c.starts_with(LC_CTYPE))
-            .and_then(|c| c.split('=').last())?
-    };
-
     // Some locales are returned with the char encoding too: `en_US.UTF-8`
+    // TODO: Once we bump MSRV >= 1.52, remove this allow and clean up
+    #[allow(clippy::manual_split_once)]
+    #[allow(clippy::needless_splitn)]
     code.splitn(2, '.').next().map(String::from)
 }
 
 #[cfg(test)]
 mod tests {
-    extern crate std;
-    use super::{get, parse_locale_code};
+    use super::{parse_locale_code, EnvAccess, _get, LANG, LC_ALL, LC_CTYPE};
+    use std::{
+        collections::HashMap,
+        ffi::{OsStr, OsString},
+    };
 
-    const LC_ALL_CONTENTS: &str = "LC_CTYPE=fr_FR.UTF-8;LC_NUMERIC=en_US.UTF-8;LC_TIME=en_US.UTF-8;LC_COLLATE=fr_FR.UTF-8;LC_MONETARY=en_US.UTF-8;LC_MESSAGES=fr_FR.UTF-8;LC_PAPER=en_US.UTF-8;LC_NAME=en_US.UTF-8;LC_ADDRESS=en_US.UTF-8;LC_TELEPHONE=en_US.UTF-8;LC_MEASUREMENT=en_US.UTF-8;LC_IDENTIFICATION=en_US.UTF-8";
-    const PARSE_LOCALE: &str = "fr_FR";
-
-    #[test]
-    fn parse_lc_all() {
-        assert_eq!(
-            parse_locale_code(LC_ALL_CONTENTS).as_deref(),
-            Some(PARSE_LOCALE)
-        );
+    type MockEnv = HashMap<OsString, String>;
+    impl EnvAccess for MockEnv {
+        fn get(&self, key: impl AsRef<OsStr>) -> Option<String> {
+            self.get(key.as_ref()).cloned()
+        }
     }
+
+    const PARSE_LOCALE: &str = "fr_FR";
 
     #[test]
     fn parse_identifier() {
@@ -67,9 +75,21 @@ mod tests {
     }
 
     #[test]
-    fn invalid_locale_doesnt_crash() {
-        std::env::set_var("LANG", "invalid");
-        std::env::set_var("LC_ALL", "invalid-again");
-        assert_eq!(get(), None);
+    fn env_priority() {
+        let mut env = MockEnv::new();
+        assert_eq!(_get(&env), None);
+
+        // These locale names are technically allowed and some systems may still
+        // defined aliases such as these but the glibc sources mention that this
+        // should be considered deprecated
+
+        env.insert(LANG.into(), "invalid".to_owned());
+        assert_eq!(_get(&env).as_deref(), Some("invalid"));
+
+        env.insert(LC_CTYPE.into(), "invalid-also".to_owned());
+        assert_eq!(_get(&env).as_deref(), Some("invalid-also"));
+
+        env.insert(LC_ALL.into(), "invalid-again".to_owned());
+        assert_eq!(_get(&env).as_deref(), Some("invalid-again"));
     }
 }
