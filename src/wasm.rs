f32d71c341bc8bc0e2a3bdfa76bdf34dfec1ec27
Oliver Dunk <oliver@1password.com>
2022-06-15 21:08:57 +0100

    Add support for workers in WASM

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,5 +1,51 @@
 use alloc::string::String;
 
+use js_sys::Object;
+use wasm_bindgen::{prelude::*, JsCast, JsValue};
+
+#[derive(Clone)]
+enum GlobalType {
+    Window(web_sys::Window),
+    Worker(web_sys::WorkerGlobalScope),
+}
+
+/// Returns a handle to the global scope object.
+///
+/// Simplified version of https://github.com/rustwasm/wasm-bindgen/blob/main/crates/js-sys/src/lib.rs,
+/// which we can't use directly because it discards information about how it
+/// retrieved the global.
+fn global() -> GlobalType {
+    #[wasm_bindgen]
+    extern "C" {
+        type Global;
+
+        #[wasm_bindgen(getter, catch, static_method_of = Global, js_class = window, js_name = window)]
+        fn get_window() -> Result<Object, JsValue>;
+
+        #[wasm_bindgen(getter, catch, static_method_of = Global, js_class = self, js_name = self)]
+        fn get_self() -> Result<Object, JsValue>;
+    }
+
+    if let Ok(window) = Global::get_window() {
+        GlobalType::Window(
+            window
+                .dyn_into::<web_sys::Window>()
+                .expect("expected window to be an instance of Window"),
+        )
+    } else if let Ok(worker) = Global::get_self() {
+        GlobalType::Worker(
+            worker
+                .dyn_into::<web_sys::WorkerGlobalScope>()
+                .expect("expected self to be an instance of WorkerGlobalScope"),
+        )
+    } else {
+        panic!("Unable to find global in this environment")
+    }
+}
+
 pub(crate) fn get() -> Option<String> {
-    web_sys::window()?.navigator().language()
+    match global() {
+        GlobalType::Window(window) => window.navigator().language(),
+        GlobalType::Worker(worker) => worker.navigator().language(),
+    }
 }
