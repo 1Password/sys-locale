1885741979e2e1b8ec2bcae300e40af040deed9c
Nathan Adams <dinnerbone@dinnerbone.com>
2023-05-30 00:32:24 +0200

    Use GetUserPreferredUILanguages on Windows to get a list instead of single locale

--- src/windows.rs
+++ src/windows.rs
@@ -1,18 +1,45 @@
-use alloc::{string::String, vec, vec::Vec};
-use windows_sys::Win32::{
-    Globalization::GetUserDefaultLocaleName, System::SystemServices::LOCALE_NAME_MAX_LENGTH,
-};
+use alloc::{string::String, vec::Vec};
+use windows_sys::Win32::Globalization::{GetUserPreferredUILanguages, MUI_LANGUAGE_NAME};
 
 #[allow(clippy::as_conversions)]
 pub(crate) fn get() -> Vec<String> {
-    let mut locale = vec![0u16; LOCALE_NAME_MAX_LENGTH as usize];
-    // SAFETY: The buffer has sufficent capacity to have locales written and is valid to write to.
-    let len = unsafe { GetUserDefaultLocaleName(locale.as_mut_ptr(), locale.len() as i32) };
-    if len > 1 {
-        let len = (len - 1) as usize;
-        if let Ok(locale) = String::from_utf16(&locale[..len]) {
-            return vec![locale];
+    let mut num_languages: u32 = 0;
+    let mut buffer_length: u32 = 0;
+
+    // Calling this with null buffer will retrieve the required buffer length
+    unsafe {
+        GetUserPreferredUILanguages(
+            MUI_LANGUAGE_NAME,
+            &mut num_languages,
+            core::ptr::null_mut(),
+            &mut buffer_length,
+        )
+    };
+
+    let mut buffer = Vec::<u16>::new();
+    buffer.resize(buffer_length as usize, 0);
+
+    // Now that we have an appropriate buffer, we can query the names
+    let mut result = Vec::with_capacity(num_languages as usize);
+    let success = unsafe {
+        GetUserPreferredUILanguages(
+            MUI_LANGUAGE_NAME,
+            &mut num_languages,
+            buffer.as_mut_ptr(),
+            &mut buffer_length,
+        )
+    } != 0;
+
+    if success {
+        // The buffer contains names split by null char (0), and ends with two null chars (00)
+        for part in buffer.split(|i| i == &0) {
+            if let Ok(locale) = String::from_utf16(part) {
+                if !locale.is_empty() {
+                    result.push(locale);
+                }
+            }
         }
     }
-    vec![]
+
+    result
 }
